2f95d0c9f48ebe96970f0c590064edb2ff0e7915
wzuhou <wuz1992@hotmail.com>
2021-02-04 12:45:06 +0100

    Update Run_make_md5.sh

--- Run_make_md5.sh
+++ Run_make_md5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Create a file >Folders ; Every row contain one name of the sample(folder names), and the fatsq files were stored in this folder.
+# Create a file “Folders” ; Every row contains one name of the sample(folder names), and the fatsq files were stored in this folder.
 for i in `less Folders` ; do sh make_md5.sh $i ;done
