a221f648000f47d0917f95eb449ee943b546790d
wzuhou <wuz1992@hotmail.com>
2022-04-04 16:17:33 +0100

    Update make_md5.sh

--- make_md5.sh
+++ make_md5.sh
@@ -3,7 +3,7 @@
 #sh Run_make_md5.sh
 #for i in `less Folders` ; do sh make_md5.sh $i ;done
 cd $1 #Enter the folder
-ls *.gz >Fastq_list #List out all the fast1 files
+ls *.gz >Fastq_list #List out all the fastq files
 for i in `less Fastq_list`;do \
 echo $i
 md5sum $i|awk '{print $1}'> ${i}.md5
